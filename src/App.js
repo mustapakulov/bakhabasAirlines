af6b190f75f1830c1aad588a5d75af224945594c
baktiar <scaletto9889@mail.ru>
2022-01-25 17:56:39 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import MyRoutes from ".MyRoutes";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <MyRoutes />;
 }
 
 export default App;

4a5a996ea7f809f328f46ce6bd5da83ad54e0afb
baktiar <scaletto9889@mail.ru>
2022-01-25 20:40:30 +0600

    .

--- src/component/Auth/Register/Register.jsx
+++ src/component/Auth/Register/Register.jsx
@@ -47,7 +47,7 @@ export default function Register() {
 
             <p>
               Don't have an account?
-              <span>Вход в аккаунт</span>
+              <span>Вход в аккаунт qweqweqwe</span>
             </p>
 
             <Link to="/login">

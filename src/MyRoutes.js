b999cb6ce212e20a430cb694808eb62e9120a5b5
zhunushov <zhunushovabbas@gmail.com>
2022-01-26 12:11:02 +0600

    ddd

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./component/Auth/Login/Login";
 import Register from "./component/Auth/Register/Register";
+import AddTicket from "./component/CRUT/AddTicket/AddTicket";
 import MyContextProvider from "./component/MyContext/MyContext";
 import MyNavbar from "./component/MyNavbar/MyNavbar";
 const MyRoutes = () => {
@@ -12,6 +13,7 @@ const MyRoutes = () => {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path='/login' element={<Login/>}/>
+          <Route path='/add' element={<AddTicket/>} />
         </Routes>
       </BrowserRouter>
     </MyContextProvider>

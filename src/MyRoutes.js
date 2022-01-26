0ed99cb8b9ba70eb9065f4f67f318d57053956b4
zhunushov <zhunushovabbas@gmail.com>
2022-01-26 10:20:06 +0600

    login

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./component/Auth/Login/Login";
 import Register from "./component/Auth/Register/Register";
 import MyContextProvider from "./component/MyContext/MyContext";
 import MyNavbar from "./component/MyNavbar/MyNavbar";
@@ -10,6 +11,7 @@ const MyRoutes = () => {
         <MyNavbar />
         <Routes>
           <Route path="/register" element={<Register />} />
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </MyContextProvider>
